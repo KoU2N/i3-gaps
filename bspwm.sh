f57a52ead93213a691e7043e4e675311c3a20eeb
KoU2N <32454313+KoU2N@users.noreply.github.com>
2019-04-02 18:47:54 +0900

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,10 +1,14 @@
-sudo apt install xorg bspwm dmenu chromium -y
+sudo apt update
+sudo apt install xorg -y
+sudo apt bspwm dmenu chromium -y
+git clone https://github.com/baskerville/bspwm ~/.
+
 mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
 
 git clone https://github.com/baskerville/bspwm ~/.
 
-cp bspwm/examples/bspwmrc ~/.config/bspwm/
-cp bspwm/examples/sxhkdrc ~/.config/sxhkd/
+cp ~/bspwm/examples/bspwmrc ~/.config/bspwm/
+cp ~/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 chmod u+x ~/.config/bspwm/bspwmrc
 chmod u+x ~/.config/sxhkd/sxhkdrc
