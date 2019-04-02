a08a1b7e4d4ec1aa1766cbe9d35756d8b6694837
KoU2N <32454313+KoU2N@users.noreply.github.com>
2019-04-02 19:57:59 +0900

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -12,3 +12,11 @@ cp ~/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp ~/bspwm/examples/sxhkdrc ~/.config/sxhkd/
 chmod u+x ~/.config/bspwm/bspwmrc
 chmod u+x ~/.config/sxhkd/sxhkdrc
+
+
+# lemonbar depends
+sudo apt install libxcb-xinerama0-dev
+sudo apt install libxcb-randr0-dev
+
+#
+sudo apt install compton
