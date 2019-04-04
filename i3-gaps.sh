304c9ce32c72be096484d4cd2c326bc4873223c2
kouunn <gao_yun@qq.com>
2019-04-05 01:17:59 +0900

    done

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -1,7 +1,7 @@
 # Installing i3-gaps
 
 ## Dependencies
-
+echo "==================install org and i3-graps dependencies=================="
 sudo apt install -y xorg libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake 
 
 
@@ -33,7 +33,10 @@ cd ~
 mkdir xx
 cd xx
 
+echo "==================i3-graps done=================="
+
 # Now i3-gaps should be installed.
+echo "==================Install i3-graps related software =================="
 sudo apt install -y feh compton i3blocks rofi zsh xbindkeys ranger 
 
 # install oh-my-zsh
@@ -46,14 +49,21 @@ git clone https://github.com/kou2n/dotfiles.git
 #git clone https://github.com/powerline/fonts.git
 
 # font 
+echo "==================Install font=================="
 sudo apt install fonts-wqy-microhei fonts-powerline fonts-font-awesome
-
+echo "==================Install input method=================="
 ### 安装fcitx google拼音 日文输入法
 sudo apt install -y fcitx fcitx-googlepinyin im-config fcitx-frontend-gtk2 fcitx-frontend-gtk3 fcitx-mozc fcitx-config-gtk fcitx-ui-classic zenity xserver-xephyr
 # 登录管理器
+echo "==================Install input lightdm=================="
 sudo apt install -y lightdm
 # 安装chromium
+echo "==================Install input chromium=================="
 sudo apt install -y chromium-browser emacs25
 # 安装spacemacs
+echo "==================Install input spacemacs=================="
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-安装
+# 安装i3配置
+echo "copy i3 config to local"
+cp -a ~/xx/dotfiles/* ~/.
+echo ""==================All done,Enjoy!!!=================="
