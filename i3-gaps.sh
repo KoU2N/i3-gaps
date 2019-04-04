c7e38c12094db43f005c1ccb7f187316fb4a62a4
kouunn <gao_yun@qq.com>
2019-04-05 01:41:27 +0900

    done

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -1,7 +1,9 @@
 # Installing i3-gaps
 
+RED='\033[0;31m'
+
 ## Dependencies
-echo "==================install org and i3-graps dependencies=================="
+echo "${RED}==================install org and i3-graps dependencies=================="
 sudo apt install -y xorg libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake 
 
 
@@ -33,10 +35,10 @@ cd ~
 mkdir xx
 cd xx
 
-echo "==================i3-graps done=================="
+echo "${RED}==================i3-graps done=================="
 
 # Now i3-gaps should be installed.
-echo "==================Install i3-graps related software =================="
+echo "${RED}==================Install i3-graps related software =================="
 sudo apt install -y feh compton i3blocks rofi zsh xbindkeys ranger 
 
 
@@ -47,25 +49,25 @@ git clone https://github.com/kou2n/dotfiles.git
 #git clone https://github.com/powerline/fonts.git
 
 # font 
-echo "==================Install font=================="
+echo "${RED}==================Install font=================="
 sudo apt install fonts-wqy-microhei fonts-powerline fonts-font-awesome
-echo "==================Install input method=================="
+echo "${RED}==================Install input method=================="
 ### 安装fcitx google拼音 日文输入法
 sudo apt install -y fcitx fcitx-googlepinyin im-config fcitx-frontend-gtk2 fcitx-frontend-gtk3 fcitx-mozc fcitx-config-gtk fcitx-ui-classic zenity xserver-xephyr
 # 登录管理器
-echo "==================Install input lightdm=================="
+echo "${RED}==================Install input lightdm=================="
 sudo apt install -y lightdm
 # 安装chromium
-echo "==================Install input chromium=================="
+echo "${RED}==================Install input chromium=================="
 sudo apt install -y chromium-browser emacs25
 # 安装spacemacs
-echo "==================Install input spacemacs=================="
+echo "${RED}==================Install input spacemacs=================="
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # 安装i3配置
-echo "copy i3 config to local"
-cp -a ~/xx/dotfiles/* ~/.
-echo ""==================All done,Enjoy!!!=================="
+echo "${RED}}==================copy i3 config to local}=================="
+cp -a ~/dotfiles/* ~/.
+echo "${RED}==================All done,Enjoy!!!=================="
 
 # install oh-my-zsh
-echo ""==================Last,install oh-my-zsh,need type password!!!=================="
+echo "${RED}==================Last,install oh-my-zsh,need type password!!!=================="
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
