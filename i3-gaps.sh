eab5111f35a6a8d8fc5552c1e80132e88dd18552
kouunn <gao_yun@qq.com>
2019-04-05 01:03:18 +0900

    done

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -40,7 +40,7 @@ sudo apt install -y feh compton i3blocks rofi zsh xbindkeys ranger
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # i3 config file
-git clone https://github.com/da-edra/dotfiles.git
+git clone https://github.com/kou2n/dotfiles.git
 # power-line-font
 # git https://github.com/gabrielelana/awesome-terminal-fonts.git
 #git clone https://github.com/powerline/fonts.git
@@ -50,5 +50,10 @@ sudo apt install fonts-wqy-microhei fonts-powerline fonts-font-awesome
 
 ### 安装fcitx google拼音 日文输入法
 sudo apt install -y fcitx fcitx-googlepinyin im-config fcitx-frontend-gtk2 fcitx-frontend-gtk3 fcitx-mozc fcitx-config-gtk fcitx-ui-classic zenity xserver-xephyr
-
+# 登录管理器
 sudo apt install -y lightdm
+# 安装chromium
+sudo apt install -y chromium-browser emacs25
+# 安装spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+安装
